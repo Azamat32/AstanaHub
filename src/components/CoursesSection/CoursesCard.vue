730055408ba42b268766a7329738d601196824c5
Azamat32 <azamat.chimchkov@gmail.com>
2024-03-29 20:40:53 +0500

    add new sections

---
+++ src/components/CoursesSection/CoursesCard.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="courses_card">
+    <div class="courses_card">
+      <div class="courses_card_top">
+        <div class="courses_top_left">
+          <span> Предпринимательство </span>
+          <h1>Freelance School</h1>
+        </div>
+        <div class="courses_top_right">
+          <span>
+            <img src="../../assets/link.svg" alt="" />
+          </span>
+          <span><img src="../../assets/star.svg" alt="" /></span>
+        </div>
+      </div>
+      <div class="courses_card_bottom">
+        <div class="courses_card_progress_line">
+          <div class="progress_bar"></div>
+        </div>
+        <p>30% материала пройдено</p>
+        <div class="bottom_btn">
+          <button>Продолжить обучение</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CoursesCard-template",
+};
+</script>
+
+<style scoped>
+.courses_card {
+  background: linear-gradient(103.57deg, #9747ff 9.31%, #7a6af4 100%);
+  border-radius: 12px;
+}
+.courses_card_top {
+  display: flex;
+  justify-content: space-between;
+  gap: 35px;
+  align-items: center;
+  padding: 16px 16px 32px 16px;
+}
+.courses_top_left span {
+  display: block;
+  font-size: 12px;
+  font-weight: 400;
+  color: #fff;
+  text-align: center;
+  border: 1px solid rgba(237, 245, 240, 1);
+  padding: 4px 8px 4px 8px;
+  border-radius: 6px;
+  margin-bottom: 16px;
+}
+.courses_top_left h1 {
+  font-size: 18px;
+  font-weight: 600;
+  color: rgba(255, 255, 255, 1);
+}
+.courses_top_right span {
+  width: 32px;
+  height: 32px;
+  padding: 16px 16px;
+  border-radius: 40px;
+  border: 1px solid rgba(255, 255, 255, 1);
+  margin-bottom: 10px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+.courses_card_progress_line {
+  width: 100%;
+  height: 3px;
+  border-radius: 16px;
+  overflow: hidden;
+  background: rgba(226, 223, 235, 1);
+  margin-bottom: 18px;
+}
+.progress_bar {
+  background: rgba(151, 71, 255, 1);
+  width: 30%;
+  height: 3px;
+  position: relative;
+  z-index: 10;
+}
+.courses_card_bottom {
+  background: #fff;
+  padding: 16px 12px 12px 12px;
+  text-align: center;
+}
+.courses_card_bottom p {
+  margin-bottom: 20px;
+  font-size: 16px;
+  font-weight: 600;
+}
+.courses_card_bottom button {
+  font-size: 16px;
+  font-weight: 600;
+  color: rgba(255, 255, 255, 1);
+  border-radius: 6px;
+  padding: 13px;
+  border: none;
+  background: rgba(151, 71, 255, 1);
+  cursor: pointer;
+  width: 100%;
+  transition: 0.4s;
+}
+.courses_card_bottom button:hover{
+  background: rgb(127, 30, 254);
+
+}
+</style>
--- src/components/CoursesSection/CoursesSection.vue
+++ src/components/CoursesSection/CoursesSection.vue
@@ -1,10 +1,92 @@
 <template>
-    <div class="courses_section" ></div>
+  <div class="courses_section">
+    <div class="courses_section_inner">
+      <div class="courses_top">
+        <CoursesCard></CoursesCard>
+        <CoursesCard></CoursesCard>
+        <CoursesCard></CoursesCard>
+      </div>
+      <div class="courses_bottom">
+        <div class="courses_bottom_inner">
+          <div class="courses_bottom_left">
+            <h1>Приведи друга получи скидку</h1>
+            <button>Прими участие в акции</button>
+          </div>
+          <div class="courses_bottom_center">
+            <img src="../../assets/bear2.svg" alt="">
+          </div>
+          <div class="courses_bottom_right">
+            <p>edu.astanahub.com/invitecode</p>
+            <button>Скопировать ссылку <img src="../../assets/screpka.svg" alt=""></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import CoursesCard from "./CoursesCard.vue";
 export default {
-    name: "CoursesSection-template",
-}
+  name: "CoursesSection-template",
+  components: {
+    CoursesCard,
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.courses_top {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 16px;
+  margin-bottom: 31px;
+}
+.courses_bottom_left{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    width: 33%;
+}
+.courses_bottom_center{
+    position: relative;
+    width: 33%;
+}
+.courses_bottom_center img{
+}
+.courses_bottom_inner {
+  background: linear-gradient(103.57deg, #9747ff 9.31%, #7a6af4 100%);
+  display: flex;
+  align-items: center;
+  padding: 23px 33px 23px 33px;
+  border-radius: 6px;
+  justify-content: space-between;
+}
+.courses_bottom_inner h1{
+font-size: 28px;
+font-weight: 800;
+text-align: center;
+margin-bottom: 9px;
+color: #fff;
+}
+.courses_bottom_inner button{
+padding: 11px 24px;
+border-radius: 6px 0px 0px 0px;
+background: rgba(232, 245, 233, 1);
+font-size: 16px;
+font-weight: 600;
+text-align: center;
+color: rgba(17, 3, 26, 1);
+display: flex;
+justify-content: center;
+align-items: center;
+border-radius: 12px;
+border: none;
+text-wrap: nowrap   ;
+gap: 4px;
+}
+.courses_bottom_right p{
+    margin-bottom: 9px;
+    color: #fff;
+    
+}
+</style>
